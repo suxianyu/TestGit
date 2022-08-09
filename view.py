a20a704b5bf6df1c5e92279d4f2a941e649f48df
suxin <1807748827@qq.com>
2022-08-09 15:22:56 +0800

    创建视图login

--- view.py
+++ view.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request)
     return HttpResponse("hello CSDN")
 
+def login(request)
+    return redirect("/index")
